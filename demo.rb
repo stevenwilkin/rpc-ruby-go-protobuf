103512b01ac70b61920ddf1bd403eb8d09e55c1e
Steven Wilkin <stevenwilkin@gmail.com>
2016-08-17 16:55:30 +0100

    Use gRPC for the demo

--- demo.rb
+++ demo.rb
@@ -3,44 +3,53 @@
 $: << '.'
 
 require 'bundler/setup'
-require 'item'
+require 'proto/items_services'
 
-Item.base_url = 'http://0.0.0.0:7000/items'
+$stub = Itemsproto::Items::Stub.new('localhost:7000', :this_channel_is_insecure)
+
+def print_item(item)
+  puts "#{item.id} - #{item.text}"
+end
 
 def list_items
   puts
   puts '> List items:'
-  Item.all.each do |item|
-    puts item
+  sorted = $stub.all(Itemsproto::Empty.new).items.sort_by { |item| item.id }
+  sorted.each do |item|
+    print_item(item)
   end
 end
 
 puts '> Create items:'
 %w{first second third forth}.each do |text|
   puts text
-  Item.create text
+  $stub.create(Itemsproto::CreateRequest.new(text: text))
 end
 
 list_items
 
 puts
 puts '> Retrieve item 2:'
-puts Item.find(2)
+print_item($stub.get(Itemsproto::GetRequest.new(id: 2)))
 
 puts
 puts '> Delete item 3:'
-puts Item.delete(3)
+$stub.delete(Itemsproto::DeleteRequest.new(id: 3))
+puts "Deleted"
 
 puts
 puts '> Delete item 3 again:'
-puts Item.delete(3)
+begin
+  $stub.delete(Itemsproto::DeleteRequest.new(id: 3))
+rescue
+  puts "Not deleted"
+end
 
 list_items
 
 puts
 puts '> Update item 2:'
-item = Item.find(2)
-item.text = 'very second'
-puts item.save
+$stub.update(Itemsproto::UpdateRequest.new(id: 2, text: 'very second'))
+puts "Updated"
 
 list_items
